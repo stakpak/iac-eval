5b7c62e6e863884b09240d54a752b79931adab02
George <kajogo777@users.noreply.github.com>
2025-03-27 08:34:40 +0200

    Update ablation-iac-eval-pipeline.py

--- evaluation/misc/ablation/ablation-iac-eval-pipeline.py
+++ evaluation/misc/ablation/ablation-iac-eval-pipeline.py
@@ -309,7 +309,8 @@ def metric_calculation_loop(composition_dict, dataset_dir, output_composition_di
                                 #         x=1
                                 reference = row["Reference output"]
                                 try:
-                                    answer, candidate = eval.separate_answer_and_code(row["LLM Output #0"], DELIMITERS)
+                                    #answer, candidate = eval.separate_answer_and_code(row["LLM Output #0"], DELIMITERS)
+                                    candidate = row["LLM Output #0"]
                                 except Exception as e:
                                     s = str(e)
                                     print(s)
